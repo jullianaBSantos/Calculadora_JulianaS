71ce82becf987f273b563bb6db921d86477d0b65
JulianaSantos <julianapremium0@gmail.com>
2025-01-23 13:30:17 +0000

    apaguei teste

--- calculadora2juliana.py
+++ calculadora2juliana.py
@@ -1,6 +1,6 @@
 def calculadora_v2(num1: float, num2: float, operador: str) -> float:
     operacoes = {
-        "**": lambda: num1 ** num2,  # Exponenciação
+        "**": lambda: num1 ** num2,  
         "+": lambda: num1 + num2,
         "-": lambda: num1 - num2,
         "/": lambda: num1 / num2,
@@ -16,14 +16,6 @@ def calculadora_v2(num1: float, num2: float, operador: str) -> float:
         return funcao()
 
     return float("nan")
-
-# Teste
-
-print(calculadora_v2(2, 3, "**"))  # 8
-print(calculadora_v2(2, 3, "+"))  # 5
-print(calculadora_v2(2, 3, "-"))  # -1
-print(calculadora_v2(2, 3, "/"))  # 0.6666666666666666
-print(calculadora_v2(2, 3, "*"))  # 6
 
 
     
